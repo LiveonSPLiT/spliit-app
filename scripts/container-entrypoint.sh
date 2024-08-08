18134b210ed80364884ac490e6295b86b1cd7fd3
Ashutosh Gupta <aashutosh.g27@gmail.com>
2024-08-08 12:31:05 +0530

    Migrated prod db to cockroachdb

--- scripts/container-entrypoint.sh
+++ scripts/container-entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Define the file path
+FILE_PATH="prisma/schema.prisma"
+
+# Check if the file exists
+if [[ -f "$FILE_PATH" ]]; then
+  # Use sed to replace the provider value
+  sed -i.bak 's/provider  = "postgresql"/provider  = "cockroachdb"/' "$FILE_PATH"
+
+  # Remove the backup file created by sed
+  rm "${FILE_PATH}.bak"
+
+  echo "Provider value has been updated in $FILE_PATH"
+else
+  echo "File $FILE_PATH does not exist."
+fi
+
 set -euxo pipefail
 
 npx prisma migrate deploy
