4022c4725a9fd49a0a9fd6358c2d663fd3fee55e
Ashutosh Gupta <aashutosh.g27@gmail.com>
2025-01-18 19:44:05 +0530

    Fixed trpc for expense comments

--- src/app/groups/[groupId]/expenses/edit-expense-form.tsx
+++ src/app/groups/[groupId]/expenses/edit-expense-form.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { RuntimeFeatureFlags } from '@/lib/featureFlags'
+import { commentFormSchema } from '@/lib/schemas'
 import { trpc } from '@/trpc/client'
 import { useRouter } from 'next/navigation'
+import { Suspense } from 'react'
+import { CommentsList } from './[expenseId]/edit/comments-list'
 import { ExpenseForm } from './expense-form'
 
 export function EditExpenseForm({
@@ -15,20 +18,30 @@ export function EditExpenseForm({
 }) {
   const { data: groupData } = trpc.groups.get.useQuery({ groupId })
   const group = groupData?.group
-
   const { data: categoriesData } = trpc.categories.list.useQuery()
   const categories = categoriesData?.categories
+
+  const { data: commentsData } = trpc.groups.expenses.comments.list.useQuery({
+    expenseId,
+  })
+  const comments = commentsData?.comments ?? []
 
   const { data: expenseData } = trpc.groups.expenses.get.useQuery({
     groupId,
     expenseId,
   })
   const expense = expenseData?.expense
-
   const { mutateAsync: updateExpenseMutateAsync } =
     trpc.groups.expenses.update.useMutation()
   const { mutateAsync: deleteExpenseMutateAsync } =
     trpc.groups.expenses.delete.useMutation()
+
+  const { mutateAsync: addExpenseCommentMutateAsync } =
+    trpc.groups.expenses.comments.create.useMutation()
+  const { mutateAsync: updateExpenseCommentMutateAsync } =
+    trpc.groups.expenses.comments.update.useMutation()
+  const { mutateAsync: deleteExpenseCommentMutateAsync } =
+    trpc.groups.expenses.comments.delete.useMutation()
 
   const utils = trpc.useUtils()
   const router = useRouter()
@@ -36,30 +49,58 @@ export function EditExpenseForm({
   if (!group || !categories || !expense) return null
 
   return (
-    <ExpenseForm
-      group={group}
-      expense={expense}
-      categories={categories}
-      onSubmit={async (expenseFormValues, participantId) => {
-        await updateExpenseMutateAsync({
-          expenseId,
-          groupId,
-          expenseFormValues,
-          participantId,
-        })
-        utils.groups.expenses.invalidate()
-        router.push(`/groups/${group.id}`)
-      }}
-      onDelete={async (participantId) => {
-        await deleteExpenseMutateAsync({
-          expenseId,
-          groupId,
-          participantId,
-        })
-        utils.groups.expenses.invalidate()
-        router.push(`/groups/${group.id}`)
-      }}
-      runtimeFeatureFlags={runtimeFeatureFlags}
-    />
+    <Suspense>
+      <ExpenseForm
+        group={group}
+        expense={expense}
+        categories={categories}
+        onSubmit={async (expenseFormValues, participantId) => {
+          await updateExpenseMutateAsync({
+            expenseId,
+            groupId,
+            expenseFormValues,
+            participantId,
+          })
+          utils.groups.expenses.invalidate()
+          router.push(`/groups/${group.id}`)
+        }}
+        onDelete={async (participantId) => {
+          await deleteExpenseMutateAsync({
+            expenseId,
+            groupId,
+            participantId,
+          })
+          utils.groups.expenses.invalidate()
+          router.push(`/groups/${group.id}`)
+        }}
+        runtimeFeatureFlags={runtimeFeatureFlags}
+      />
+      <CommentsList
+        group={group}
+        expense={expense}
+        comments={comments}
+        onCreate={async (values: unknown, participantId: string) => {
+          const commentFormValues = commentFormSchema.parse(values)
+          await addExpenseCommentMutateAsync({
+            expenseId: expense!.id,
+            participantId,
+            text: commentFormValues.comment,
+          })
+          utils.groups.expenses.comments.list.invalidate({ expenseId })
+        }}
+        onUpdate={async (values: unknown, commentId: string) => {
+          const commentFormValues = commentFormSchema.parse(values)
+          await updateExpenseCommentMutateAsync({
+            commentId,
+            text: commentFormValues.comment,
+          })
+          utils.groups.expenses.comments.list.invalidate({ expenseId })
+        }}
+        onDelete={async (commentId: string) => {
+          await deleteExpenseCommentMutateAsync({ commentId })
+          utils.groups.expenses.comments.list.invalidate({ expenseId })
+        }}
+      />
+    </Suspense>
   )
 }
