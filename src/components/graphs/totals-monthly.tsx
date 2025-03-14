edf46c07fc5479ccc76c8917fc5ecfbfc7211291
Ashutosh Gupta <aashutosh.g27@gmail.com>
2025-03-14 15:01:46 +0530

    Updated Bar graph

--- src/components/graphs/totals-monthly.tsx
+++ src/components/graphs/totals-monthly.tsx
@@ -7,28 +7,13 @@ import {
 } from '@/components/ui/card'
 import { getMonthlySpendingData } from '@/lib/dashboardApi'
 import { useTranslations } from 'next-intl'
-import { VictoryChart, VictoryBar, VictoryTheme } from 'victory'
+import { VictoryAxis, VictoryChart, VictoryBar, VictoryTheme } from 'victory'
 
 type Props = {
     totalMonthlyExpense: NonNullable<
     Awaited<ReturnType<typeof getMonthlySpendingData>>
   >
 }
-
-const colorMap = [
-  '#45b29d',
-  '#334d5c',
-  '#45b29d',
-  '#efc94c',
-  '#e27a3f',
-  '#df5a49',
-  '#4f7da1',
-  '#efda97',
-  '#e2a37f',
-  '#df948a',
-  '#efda97',
-  '#3f51b5',
-];
 
 export function MonthySummary({ totalMonthlyExpense }: Props) {
   const t = useTranslations('Dashboard')
@@ -42,21 +27,19 @@ export function MonthySummary({ totalMonthlyExpense }: Props) {
       <CardContent className="flex flex-col space-y-4 pb-0">
         <VictoryChart
                       domainPadding={{ x: 20 }}
-                      theme={VictoryTheme.grayscale}
+                      theme={VictoryTheme.clean}
                     >
                       <VictoryBar
                       colorScale={"qualitative"}
                       x="month"
                       y="total"
-                      style={{
-                        data: {
-                          fill: ({ index }) =>
-                            typeof index === 'number'
-                              ? colorMap?.reverse()[index as number]
-                            : colorMap[0],
-                          },
-                    }}
                       data={totalMonthlyExpense} />
+                      <VictoryAxis style={{
+                        tickLabels: {fill: '#455A64'}
+                      }}/> 
+                      <VictoryAxis dependentAxis style={{
+                        tickLabels: {fill: '#455A64'}
+                      }}/>
                     </VictoryChart>
         
       </CardContent>
